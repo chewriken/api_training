91420ec8489e44e523a919b5a654f4dd92af363f
Joakim Marie-Sainte <marie-sainte.j@hotmail.com>
2021-03-24 14:10:02 +0100

    Intregration de l'api agify dans la recherche de matches

--- src/main/java/fr/esiea/ex4A/inscription/InscriptionController.java
+++ src/main/java/fr/esiea/ex4A/inscription/InscriptionController.java
@@ -2,12 +2,9 @@ package fr.esiea.ex4A.inscription;
 
 
 import org.springframework.web.bind.annotation.*;
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,36 +14,25 @@ public class InscriptionController {
     public final HashMap <String,UserInfo> userMap = new HashMap<>();
     public final HashMap <String,AgifyInfo> agifyMap = new HashMap<>();
     public final AgifyClient agifyClient;
+    public final Matches matches;
 
-    public InscriptionController(AgifyClient agifyClient) {
+    public InscriptionController(AgifyClient agifyClient, Matches matches) {
         this.agifyClient = agifyClient;
+        this.matches = matches;
     }
 
     @PostMapping("/api/inscription")
-    public void inscription(@RequestBody UserInfo userInfo) throws InterruptedException, IOException {
-        Call<AgifyInfo> call = agifyClient.defineAge(userInfo.name, userInfo.pays);
-        call.enqueue(new Callback<AgifyInfo>() {
-            @Override
-            public void onResponse(Call<AgifyInfo> call, Response<AgifyInfo> response) {
-                agifyMap.put(response.body().name,response.body());
-            }
-
-            @Override
-            public void onFailure(Call<AgifyInfo> call, Throwable t) {
-
-            }
-        });
+    public void inscription(@RequestBody UserInfo userInfo) throws IOException {
+        Response<AgifyInfo> response = agifyClient.defineAge(userInfo.name, userInfo.pays).execute();
+        AgifyInfo agifyInfo = response.body();
+        agifyMap.put(agifyInfo.name,agifyInfo);
         userMap.put(userInfo.name,userInfo);
         System.out.println(userMap);
     }
 
     @GetMapping("/api/matches")
     public List<UserInfo> matchesUser(@RequestParam(value="userName") String name){
-        //final List<UserInfo> listUser = matches.getMatches(userMap, agifyMap,name);
-        final List<UserInfo> listUser = new ArrayList<>();
-        for(UserInfo i : userMap.values()){
-            listUser.add(i);
-        }
+        final List<UserInfo> listUser = matches.getMatches(userMap, agifyMap,name);
         return listUser;
     }
 }
---
+++ src/main/java/fr/esiea/ex4A/inscription/Matches.java
@@ -0,0 +1,28 @@
+package fr.esiea.ex4A.inscription;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class Matches {
+
+    public List<UserInfo> getMatches(HashMap<String,UserInfo> userMap, HashMap<String,AgifyInfo> agifyMap,String name) {
+        final AgifyInfo userAgify = agifyMap.get(name);
+        final UserInfo userInfo = userMap.get(name);
+        final List<UserInfo> matches = new ArrayList<>();
+        for(AgifyInfo i : agifyMap.values()){
+            if(i.age<= userAgify.age+4 || i.age>= userAgify.age-4){
+                UserInfo potentialMatch = userMap.get(i.name);
+                //System.out.println(potentialMatch);
+                if(potentialMatch.sexe.equals(userInfo.sexPref) && userInfo.twitter!= potentialMatch.twitter && userInfo.name!=potentialMatch.name){
+                    matches.add(userMap.get(i.name));
+                }
+            }
+        }
+        return matches;
+    }
+
+}
